b4153e56deb8b9c42ab7b977d7bc0a4b8951fb69
Jingjing-20 <jingjing052704@gmail.com>
2026-08-19 20:48:43 +0800

    detailing 42

--- src/components/resume_sections/carddev/contact_data.jsx
+++ src/components/resume_sections/carddev/contact_data.jsx
@@ -2,7 +2,7 @@ import ResumeImg from '@/components/resume_sections/carddev/resume.webp';
 import LinkedInImg from '@/components/resume_sections/carddev/linkedin.webp';
 import GitHubImg from '@/components/resume_sections/carddev/github.webp';
 import GmailImg from '@/components/resume_sections/carddev/gmail.webp';
-import ResumePDF from '@/components/resume_sections/carddev/resumes/GCNU - RESUME v11.pdf';
+import ResumePDF from '@/components/resume_sections/carddev/resumes/GCNU - RESUME v12.pdf';
 
 export const CONTACT_LINKS = [
   {
