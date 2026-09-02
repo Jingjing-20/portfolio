4b84972e38dcbaf3f7d0d52b6e0b87e9bd3eeaf2
Jingjing-20 <jingjing052704@gmail.com>
2026-09-02 17:46:50 +0800

    adjustments 14

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -142,6 +142,9 @@ export default function About() {
             <span className="hidden md:block text-[8px] md:text-[10px] font-medium text-base-content">
               {contact.label}
             </span>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><title>open</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 4H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-4m-8-2l8-8m0 0v5m0-5h-5"/></svg>
+            </span>
           </button>
         ))}
       </nav>
